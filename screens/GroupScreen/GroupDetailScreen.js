ef54901a3a62bbcee73f1423ef3a8771bd76f0e3
Thai Huy Nhat Quang <thaihuynhatquang@gmail.com>
2019-07-22 14:02:29 +0700

    A few changes in Chat Screen Information

--- screens/GroupScreen/GroupDetailScreen.js
+++ screens/GroupScreen/GroupDetailScreen.js
@@ -5,6 +5,7 @@ import { Camera } from 'expo-camera';
 import * as Permissions from 'expo-permissions';
 import * as ImagePicker from 'expo-image-picker';
 import RNPickerSelect from 'react-native-picker-select';
+import * as NativeBaseComponent from 'native-base';
 
 import { categoryPlaces } from '../../data/SampleData';
 import TextSize from '../../constants/TextSize';
@@ -12,12 +13,20 @@ import Colors from '../../constants/Colors';
 import Layout from '../../constants/Layout';
 
 export default class GroupDetailScreen extends Component {
+  state = {
+    image: null,
+    hasExplorePermission: null,
+    type: Camera.Constants.Type.back,
+    editMode: false,
+    groupName: 'Group Name',
+    textInputValue: 'Eating',
+    description: 'Nothing here',
+  };
+
   static navigationOptions = ({ navigation }) => {
+    const { state } = navigation;
     return {
       title: 'Groups Detail',
-      headerStyle: {
-        // backgroundColor: Colors.tintColor,
-      },
       headerTitleStyle: {
         fontWeight: 'bold',
         fontSize: TextSize.TEXT_TITLE,
@@ -36,30 +45,23 @@ export default class GroupDetailScreen extends Component {
         />
       ),
       headerRight: (
-        <Icon
-          name={Platform.OS === 'ios' ? 'ios-create' : 'md-create'}
-          type='ionicon'
-          size={28}
-          color={Colors.tintColor}
-          iconStyle={{
-            marginRight: 15,
-          }}
-          onPress={() => {
-            console.log(state.editMode);
-          }}
-        />
+        <View style={{ justifyContent: 'center', marginRight: 5 }}>
+          <NativeBaseComponent.Button
+            onPress={() => navigation.getParam('handleMode', {})()}
+            small
+            rounded
+            style={{ backgroundColor: Colors.tintColor }}>
+            <NativeBaseComponent.Text>
+              {navigation.getParam('editMode', false) === true ? 'Save' : 'Edit'}
+            </NativeBaseComponent.Text>
+          </NativeBaseComponent.Button>
+        </View>
       ),
     };
   };
-  state = {
-    image: null,
-    hasExplorePermission: null,
-    type: Camera.Constants.Type.back,
-    editMode: false,
-    textInputValue: '',
-  };
 
   async componentDidMount() {
+    await this.props.navigation.setParams({ handleMode: () => this.switchMode() });
     const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
     this.setState({ hasExplorePermission: status === 'granted' });
   }
@@ -82,14 +84,15 @@ export default class GroupDetailScreen extends Component {
     return Promise.reject();
   };
 
-  switchMode = () => {
-    this.setState({
-      editMode: !editMode,
+  switchMode = async () => {
+    await this.setState({
+      editMode: !this.state.editMode,
     });
+    this.props.navigation.setParams({ editMode: this.state.editMode });
   };
 
   render() {
-    const { image } = this.state;
+    const { image, editMode } = this.state;
     const placeholder = {
       label: 'Select category place...',
       value: null,
@@ -104,30 +107,36 @@ export default class GroupDetailScreen extends Component {
                 this._pickImageFromExplorer()
                   .then((r) => console.log(r))
                   .catch((e) => console.log(e))
-              }>
+              }
+              disabled={!editMode}>
               {image ? (
-                <Image style={styles.avatar} source={{ uri: image }} />
+                <Image
+                  style={[styles.avatar, { borderColor: editMode ? 'white' : Colors.disableInputBackground }]}
+                  source={{ uri: image }}
+                />
               ) : (
                 <Image
-                  style={styles.avatar}
+                  style={[styles.avatar, { borderColor: editMode ? 'white' : Colors.disableInputBackground }]}
                   source={{ uri: 'https://uphinh.org/images/2019/07/18/Untitled-16446da271a5f9b7c.png' }}
                 />
               )}
             </TouchableOpacity>
             <Input
-              inputStyle={styles.name}
-              placeholder='Group name'
-              placeholderTextColor={Colors.placeHolderLightColor}
+              inputStyle={[styles.name, { color: editMode ? '#FFFFFF' : Colors.silver }]}
+              value={this.state.groupName}
+              editable={editMode}
             />
           </View>
         </View>
         <View style={{ flex: 2, justifyContent: 'flex-start', paddingHorizontal: 30 }}>
           <Text style={{ marginTop: 15, marginBottom: 15, fontWeight: 'bold' }}>Description</Text>
           <TextInput
-            style={Platform.OS === 'ios' ? pickerSelectStyles.inputIOS : pickerSelectStyles.inputAndroid}
+            style={[styles.textInput, { backgroundColor: editMode ? 'white' : Colors.disableInputBackground }]}
             onChangeText={(text) => this.setState({ text })}
-            value={this.state.text}
+            value={this.state.description}
             placeholder='Describe something about this...'
+            placeholderTextColor={Colors.placeHolderLightColor}
+            editable={editMode}
           />
           <Text style={{ marginTop: 15, marginBottom: 15, fontWeight: 'bold' }}>Category</Text>
           <RNPickerSelect
@@ -138,8 +147,13 @@ export default class GroupDetailScreen extends Component {
                 textInputValue: value,
               });
             }}
-            style={pickerSelectStyles}
+            style={{
+              inputIOS: [styles.textInput, { backgroundColor: editMode ? 'white' : Colors.disableInputBackground }],
+              inputAndroid: [styles.textInput, { backgroundColor: editMode ? 'white' : Colors.disableInputBackground }],
+            }}
             value={this.state.textInputValue}
+            placeholderTextColor={Colors.placeHolderLightColor}
+            disabled={!editMode}
           />
           <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Button
@@ -171,12 +185,10 @@ const styles = StyleSheet.create({
     height: 130,
     borderRadius: 63,
     borderWidth: 4,
-    borderColor: 'white',
     marginBottom: 10,
   },
   name: {
     fontSize: 22,
-    color: '#FFFFFF',
     fontWeight: '600',
     textAlign: 'center',
   },
@@ -199,27 +211,21 @@ const styles = StyleSheet.create({
     shadowRadius: 9,
     elevation: 14,
   },
-});
-
-const pickerSelectStyles = StyleSheet.create({
-  inputIOS: {
+  textDecription: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    color: 'black',
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+  textInput: {
     fontSize: 16,
     paddingVertical: 12,
     paddingHorizontal: 10,
     borderWidth: 1,
     borderColor: 'gray',
-    borderRadius: 4,
     color: 'black',
-    paddingRight: 30, // to ensure the text is never behind the icon
-  },
-  inputAndroid: {
-    fontSize: 16,
-    paddingHorizontal: 10,
-    paddingVertical: 8,
-    borderWidth: 0.5,
-    borderColor: 'gray',
     borderRadius: 4,
-    color: 'black',
     paddingRight: 30, // to ensure the text is never behind the icon
   },
 });
